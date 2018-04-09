dba2afe604754251d42e8b6c5e12581b84148f0e
neuromozg <spacemax@yandex.ru>
2018-04-09 17:30:31 +0300

    добавил HSV_filter.py

--- test_opencv.py
+++ test_opencv.py
@@ -17,16 +17,21 @@ while True:
         _, thresh = cv2.threshold(blur, gain, 255, cv2.THRESH_BINARY_INV)
 
         #находим контуры
-        _, contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        _, contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_TREE,
+                                          cv2.CHAIN_APPROX_SIMPLE)
         
         if contours:
-
             # берем максимальный контур по периметру
             mainContour = max(contours, key = cv2.contourArea)
-            M = cv2.moments(mainContour)  #находим моменты
-            if M['m00'] != 0:   # если нет деления на ноль
-                cx = int(M['m10']/M['m00'])     # смотрим координаты центра наибольшего черного пятна
-                cy = int(M['m01']/M['m00'])     # они получаются в пикселях кадра
+            M = cv2.moments(mainContour)  #находим моменты контура
+
+            dArea = M['m00'] #момент нулевого порядка, количество точек в контуре
+            sumX = M['m10'] #момент первого порядка, сумма всех координат X точек контура
+            sumY = M['m01'] #момент первого порядка, сумма всех координат Y точек контура
+            
+            if dArea != 0:   # если нет деления на ноль
+                cx = int(sumX/dArea)     # вычисляем координаты центра контура
+                cy = int(sumY/dArea)     # они получаются в пикселях кадра
 
                 # рисуем перекрестье на контуре
                 cv2.line(frame, (cx, 0), (cx, 1280), (255, 0, 0), 1)
@@ -34,19 +39,21 @@ while True:
             
             #отрисовываем контур на исходной картинке
             cv2.drawContours(frame, mainContour, -1, (0, 255, 0), 2, cv2.FILLED) #отображаем контуры на изображении
+            cv2.putText(frame, 'Gain = %d' % gain, (10, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                        1, (0,255,255), 1)
 
         
-        cv2.imshow('img1', frame) #отображаем кадр
+        cv2.imshow('frame', frame) #отображаем кадр
+        #cv2.imshow('gray', gray) #отображаем кадр
+        #cv2.imshow('thresh', thresh) #отображаем кадр
 
         key = cv2.waitKey(1) & 0xFF  #ждем нажатия клавиши
         if key == ord('q'): #если нажата Q тогда выходим
             break
         elif key == 45: #нажали "-"
             gain -= 1
-            print ('Gain: %d' % gain)
         elif key == 43: #нажали "+"
             gain += 1
-            print ('Gain: %d' % gain)
     else:
         break
 
